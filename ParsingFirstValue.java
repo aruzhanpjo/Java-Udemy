5aed49b0d6375c8ab1fa239bd67285ddea06bd6b
Aruzhan Betigenova <aruzhan.betigenova@mnsu.edu>
2023-06-11 21:34:24 -0500

    added another method & cut the number of lines

--- ParsingFirstValue.java
+++ ParsingFirstValue.java
@@ -38,12 +38,45 @@ public class ParsingFirstValue {
         System.out.println("Oh, ok. Hello, " + name + ". That's wassup. ");
 
         System.out.println("What year were you born? ");
-        String dob = scanner.nextLine();
-        int age = CurrentYear - Integer.parseInt(dob);
+        //String dob = scanner.nextLine();
+        //int age = CurrentYear - Integer.parseInt(dob);
+
+        boolean validDOB = false;
+        int age = 0;
+
+        do {
+            System.out.println("Enter your year of birth that is bigger than or equal to "
+            + (CurrentYear-130));
+
+            /*
+            instead put these lines already inside & just call a method
+
+            String dob = scanner.nextLine();
+            age = CurrentYear - Integer.parseInt(dob);
+             */
+            age = checkData(CurrentYear, scanner.nextLine());
+            validDOB = age<0?false:true;
+            
+        } while (!validDOB);
+
+
         return "Gotcha. So you're " + age + " years old. That's wassup. ";
         } finally {
             scanner.close();
         }
+
+
         
+    }
+
+    public static int checkData(int currentYear, String dob) {
+        int dob1 = Integer.parseInt(dob);
+        int minYear = currentYear - 130;
+
+        if ((dob1 < minYear) || (dob1 > currentYear)) {
+            return -1;
+        }
+
+        return (currentYear - dob1);
     }
 }
