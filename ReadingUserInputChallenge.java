208bd5e53cf87187c2aff8407b16df51a03f11e2
Aruzhan Betigenova <aruzhan.betigenova@mnsu.edu>
2023-06-13 15:20:35 -0500

    Input challenge

---
+++ ReadingUserInputChallenge.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class ReadingUserInputChallenge {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int sum = 0;
+        int counter = 1;
+
+        while (counter<=5) {
+            System.out.println("Enter number #" + counter + ":");
+            String nextN = scanner.nextLine();
+            try {
+                int number = Integer.parseInt(nextN);
+                counter++;
+                sum+=number;
+            } catch (NumberFormatException noCharacters) {
+                System.out.println("Please, no characters. Only numbers!");
+            }
+        }
+        System.out.println("The sum of the numbers is " + sum);
+    }
+}
