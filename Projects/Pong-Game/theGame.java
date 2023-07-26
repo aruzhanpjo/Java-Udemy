0b80a28d9eb175ef73d2cb21de02b697864c1f7e
aruzhanpjo <aruzhan.betigenova@mnsu.edu>
2023-07-26 17:38:12 -0500

    the main class that executes the game itself

---
+++ Projects/Pong-Game/theGame.java
@@ -0,0 +1,12 @@
+import java.awt.*;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+
+public class theGame {
+    
+    public static void main(String[] args) {
+        
+        GameFrame frame = new GameFrame();
+    }
+}
